95d8e702bedbe8d35edcc6c337a80a806b039045
Saurabh Singh <saurabhrajpoot1999gmail.com>
2022-09-15 01:38:00 +0530

    Added login and register page

--- inventory_management_system/accounts/urls.py
+++ inventory_management_system/accounts/urls.py
@@ -2,9 +2,14 @@ from django.contrib import admin
 from accounts.views import login_page
 from django.urls import path,include
 from django.conf.urls.static import static
+from accounts import views
 # from django.conf import settings
 # from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
-    path('', login_page, name="login")
+    path('', views.login_page, name="login"),
+    path('login1', views.loginpage, name="login1"),
+    path('register', views.registerpage, name="register"),
+    
 ]
+
--- inventory_management_system/accounts/views.py
+++ inventory_management_system/accounts/views.py
@@ -1,7 +1,45 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from django.contrib.auth.models import User
 
 # Create your views here.
 
 
 def login_page(request):
     return render(request,'accounts/login.html')
+
+
+def loginpage(request):
+    if request.method=='POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password1 = request.POST['password1']
+        password2 = request.POST['passowrd2']
+
+        myuser = User.objects.create_user(username,email,password1)
+        myuser.save()
+        massages.success(request, "Your Account Successfully created.")
+        
+    else:
+        return HttpResponse('404 - Not Found')
+    context = {}
+    return render(request,'accounts/login1.html',context)
+
+
+def registerpage(request):
+    if request.method=='POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password1 = request.POST['password1']
+        password2 = request.POST['passowrd2']
+
+        myuser = User.objects.create_user(username,email,password1)
+        myuser.save()
+        massages.success(request, "Your Account Successfully created.")
+        
+    else:
+        return HttpResponse('404 - Not Found')
+        
+    context = {}
+    return render(request,'accounts/login1.html',context)
+    context = {}
+    return render(request,'accounts/register.html',context)
